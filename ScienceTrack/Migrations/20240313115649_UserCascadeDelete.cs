f359712862c3f5c743808bcde2b167c1d8ae8468
Илья Кабанов <kabanow.g@gmail.com>
2024-03-13 17:12:04 +0500

    new migrations

---
+++ ScienceTrack/Migrations/20240313115649_UserCascadeDelete.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScienceTrack.Migrations
+{
+    public partial class UserCascadeDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "GameUsers_user_fkey",
+                table: "GameUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "RoundUsers_user_fkey",
+                table: "RoundUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "GameUsers_user_fkey",
+                table: "GameUsers",
+                column: "user",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "RoundUsers_user_fkey",
+                table: "RoundUsers",
+                column: "user",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "GameUsers_user_fkey",
+                table: "GameUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "RoundUsers_user_fkey",
+                table: "RoundUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "GameUsers_user_fkey",
+                table: "GameUsers",
+                column: "user",
+                principalTable: "Users",
+                principalColumn: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "RoundUsers_user_fkey",
+                table: "RoundUsers",
+                column: "user",
+                principalTable: "Users",
+                principalColumn: "id");
+        }
+    }
+}
---
+++ ScienceTrack/Migrations/20240313120154_NullableUserInGameAndRound.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScienceTrack.Migrations
+{
+    public partial class NullableUserInGameAndRound : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "user",
+                table: "RoundUsers",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "user",
+                table: "GameUsers",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "user",
+                table: "RoundUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "user",
+                table: "GameUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
